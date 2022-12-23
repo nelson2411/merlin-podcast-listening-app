1bc58ba4a191004213e6cb8aebac400a0086a0d9
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-23 15:39:11 +0100

    feat: global styles

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -1,1 +1,17 @@
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
+
+/* 
+On this component we define the global styles for the Podcaster App
+*/
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: teal;
+  margin: 0;
+  padding: 0;
+  
+}
+
+`
+
+export default GlobalStyle
