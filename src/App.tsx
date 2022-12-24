a8373e6374e99b7c0e627a661c39bd0612f66fdd
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-24 14:31:33 +0100

    feat: new route created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom"
 import "./App.css"
 import Home from "./pages/home/Home"
 import OnePodcastInfo from "./pages/solo-podcast/OnePodcastInfo"
+import SoloEpisode from "./pages/solo-episode/SoloEpisode"
 import GlobalStyle from "./components/GlobalStyle"
 
 function App() {
@@ -12,6 +13,11 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/podcast/:id" component={OnePodcastInfo} />
+        <Route
+          exact
+          path="/podcast/:id/episode/:trackId"
+          component={SoloEpisode}
+        />
       </Switch>
     </div>
   )
